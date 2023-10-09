71c284b1cc6374ac4ad27c70b4decc376dadebbd
Kirill <kirill_ozornin@mail.ru>
2023-10-09 20:10:29 +0200

    add online friends

---
+++ src/components/closeFridns/CloseFriends.tsx
@@ -0,0 +1,13 @@
+import "./closeFriends.css"
+
+export default function CloseFriends() {
+   const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+   return (
+      <div>
+         <li className="sidebarFriend">
+            <img className="sidebarFriendImg" src={'assets/person/person0.jpeg'} alt="" />
+            <span className="sidebarFriendName"></span>
+         </li>
+      </div>
+   )
+}
